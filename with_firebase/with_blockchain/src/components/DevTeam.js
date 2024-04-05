4240570c20ed6af6a1f351a9c691eabd24af773c
Shiva Krishna <71482277+shivachandapu@users.noreply.github.com>
2024-04-05 13:54:11 +0530

    Delete with_firebase/with_blockchain directory

--- with_firebase/with_blockchain/src/components/DevTeam.js
+++
@@ -1,9 +0,0 @@
-export const DevTeam = [
-	{
-		id: 0,
-		name: 'Shiva Krishna',
-		imgUrl: 'https://avatars.githubusercontent.com/u/71482277?s=400&u=7b4cab646d2c1fa86e7cfc1e855d8d8ded26707e&v=4',
-		githubUrl: 'https://github.com/shivachandapu',
-		linkedInUrl: 'https://linkedin.com/in/chandapu-shiva-krishna',
-	},
-];
